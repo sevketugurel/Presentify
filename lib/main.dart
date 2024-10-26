6e81a11e67355c0ad69a5eb3c8e71db1452b4708
Sevket Ugurel <ugurelsevket@gmail.com>
2024-10-26 19:18:27 +0300

    FireBase integration done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
